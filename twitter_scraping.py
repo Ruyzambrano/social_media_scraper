fc6a4a2bb789a7516a5dfc4133be5a3eaacac257
ruyzambrano <ruyzambrano@gmail.com>
2024-08-02 12:39:38 +0100

    Implementing Reddit scraper

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -4,29 +4,15 @@ from tweepy import API, OAuth1UserHandler, Forbidden
 from functions import get_secrets, configure_logging, write_log_message
 
 
-
-
-
 def authenticate_tweepy(secrets: dict) -> API:
-    """Gets an instance of a Twitter API with authentification"""
+    """Gets an instance of a Twitter API with authentication"""
     auth = OAuth1UserHandler(
         secrets["TWITTER_API_KEY"],
         secrets["TWITTER_API_SECRET_KEY"],
-        secrets["TWITTER_ACCESS_TOKEN"], 
+        secrets["TWITTER_ACCESS_TOKEN"],
         secrets["TWITTER_ACCESS_TOKEN_SECRET"]
     )
     return API(auth)
-
-from socialreaper import Twitter
-from socialreaper.tools import to_csv
-
-twt = Twitter(app_key="xxx", app_secret="xxx", oauth_token="xxx", 
-    oauth_token_secret="xxx")
-    
-tweets = twt.user("realDonaldTrump", count=500, exclude_replies=True, 
-    include_retweets=False)
-    
-to_csv(list(tweets), filename='trump.csv')
 
 
 if __name__ == "__main__":
@@ -34,12 +20,13 @@ if __name__ == "__main__":
 
     env = get_secrets(twitter_logger)
 
-    # try:
-    #     app = authenticate_tweepy(env)
-    #     print(app.get_user(screen_name="ruyzambrano"))
+    try:
 
-    # except Forbidden as e:
-    #     for index, code in enumerate(e.api_codes):
-    #         print(f"{code}: {e.api_messages[index]}")
+        app = authenticate_tweepy(env)
+        print(app.get_user(screen_name="elonmusk"))
+
+    except Forbidden as e:
+        for index, code in enumerate(e.api_codes):
+            print(f"{code}: {e.api_messages[index]}")
 
     write_log_message(twitter_logger, "Finish run\n")
