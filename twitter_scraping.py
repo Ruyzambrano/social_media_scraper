9d05f6c95edf5c2f4a0bb54b83691c3acdfe8d58
ruyzambrano <ruyzambrano@gmail.com>
2024-08-02 12:26:47 +0100

    Removed logs

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -4,6 +4,9 @@ from tweepy import API, OAuth1UserHandler, Forbidden
 from functions import get_secrets, configure_logging, write_log_message
 
 
+
+
+
 def authenticate_tweepy(secrets: dict) -> API:
     """Gets an instance of a Twitter API with authentification"""
     auth = OAuth1UserHandler(
@@ -14,17 +17,29 @@ def authenticate_tweepy(secrets: dict) -> API:
     )
     return API(auth)
 
+from socialreaper import Twitter
+from socialreaper.tools import to_csv
+
+twt = Twitter(app_key="xxx", app_secret="xxx", oauth_token="xxx", 
+    oauth_token_secret="xxx")
+    
+tweets = twt.user("realDonaldTrump", count=500, exclude_replies=True, 
+    include_retweets=False)
+    
+to_csv(list(tweets), filename='trump.csv')
+
 
 if __name__ == "__main__":
     twitter_logger = configure_logging("twitter.log")
 
     env = get_secrets(twitter_logger)
-    try:
-        app = authenticate_tweepy(env)
-        print(app.get_user(screen_name="ruyzambrano"))
-        
-    except Forbidden as e:
-        for index, code in enumerate(e.api_codes):
-            print(f"{code}: {e.api_messages[index]}")
-    finally:
-        write_log_message(twitter_logger, "Finish run\n")
+
+    # try:
+    #     app = authenticate_tweepy(env)
+    #     print(app.get_user(screen_name="ruyzambrano"))
+
+    # except Forbidden as e:
+    #     for index, code in enumerate(e.api_codes):
+    #         print(f"{code}: {e.api_messages[index]}")
+
+    write_log_message(twitter_logger, "Finish run\n")
